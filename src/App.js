e6fe4b0ae9b5787ee554afbf4782aa4b7cc71507
Nikhil <nikhilgarware1198@gmail.com>
2020-12-06 14:37:32 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Greet from './components/Greet'
+
+function App() {
+  return (
+    <div className="App">
+        <Greet name="Mark" sub="Python">
+            <p>This is a childeren conponent of Greet. If you want to use this eliminate 
+              the Greet Tag and put the Greet closing tag and then after that close the 
+              Greet tag and make changes in the Greet.js file.
+              </p>
+          </Greet>
+        <Greet name="David" sub="DS"/> 
+        <Greet name="Ruby" sub = "C++"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Greet= props =>{
+    console.log(props)
+    return (
+        <div>
+            <h1>Hello, {props.name} are you intrested in {props.sub} ?</h1>
+            {props.children}
+        </div>
+    )
+}
+export default Greet
